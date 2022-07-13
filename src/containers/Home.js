c8d43c380adc6025a5300ab7a3b6a2286f33a203
Hanif <hanif.hakim@spesolution.com>
2022-07-13 09:08:56 +0700

    logout and display user

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../config/firebase";
 
 const Home = () => {
+  const [user, loading, error] = useAuthState(auth);
   return (
-    <div style={{margin: '10px'}}>Home</div>
-  )
-}
+    <div style={{ margin: "10px" }}>
+      Welocome to Mood Meter! <br />
+      Current User :{user.email}
+    </div>
+  );
+};
 
-export default Home
+export default Home;
